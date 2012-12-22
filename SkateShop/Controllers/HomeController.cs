64982639f49794e061171735dbd462b8cbd47145
Igor Goremykin <c-sharp@bk.ru>
2012-12-22 22:48:47 +0200

    First commit

---
+++ SkateShop/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SkateShop.Models;
+
+namespace SkateShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
